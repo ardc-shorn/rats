da1dd301581de5a2de31a18c7f0da07e8a71553f
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2022-06-16 16:42:09 +1000

    move import to top

--- src/RaidApi.ts
+++ src/RaidApi.ts
@@ -1,5 +1,6 @@
-const apiBase = "https://api.raid.org.au/v1";
 import fetch from "node-fetch";
+
+const apiBase = "https://api.raid.org.au/v1";
 
 export async function getRoot(): Promise<any> {
   const response = await fetch(`${apiBase}/`, {
